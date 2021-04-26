ef6ed8f42fea562cba5433620957f944c5afcef6
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-04-26 22:58:26 +0800

    数值列表

---
+++ 4/range.py
@@ -0,0 +1,15 @@
+for value in range(1, 5):
+    print(value)
+
+print()
+
+# 设置步长
+# 10 以内的所有偶数
+for value in range(2, 11, 2):
+    print(value)
+
+print()
+
+# 10 以内的整数的平方
+for value in range(1, 11):
+    print(value ** 2)

68ed571fb4ebcd2ed3452df7e1191812be5fbfc5
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-04-26 20:28:52 +0800

    删除空白

--- 2/strip.py
+++ 2/strip.py
@@ -3,8 +3,10 @@ favorite_language = ' python '
 print(favorite_language)
 print(favorite_language.rstrip())
 print(favorite_language.rstrip().lstrip())
+print(favorite_language.strip())
 
 favorite_language = '\npython\t'
 print(favorite_language)
 print(favorite_language.rstrip())
-print(favorite_language.rstrip().lstrip())
+print(favorite_language.lstrip())
+print(favorite_language.strip())

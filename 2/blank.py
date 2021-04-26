b1eb9983821f2dde1f47ea087ff53a863e2f8abe
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-04-26 20:27:05 +0800

    删除空白

---
+++ 2/blank.py
@@ -0,0 +1,10 @@
+favorite_language = ' python '
+
+print(favorite_language)
+print(favorite_language.rstrip())
+print(favorite_language.rstrip().lstrip())
+
+favorite_language = '\npython\t'
+print(favorite_language)
+print(favorite_language.rstrip())
+print(favorite_language.rstrip().lstrip())

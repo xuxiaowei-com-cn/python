d5d8786e2afafa5141b5e8394efd1a652c96e736
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-04-26 20:10:13 +0800

    在字符串中使用变量

--- 2/f.py
+++ 2/f.py
@@ -1,3 +1,6 @@
+# f 字符串时 Python 3.6 引入的。
+# Python 3.5 或更早的版本没需要使用 format() 方法
+
 first_name = "ada"
 last_name = "lovelace"
 full_name = f"{first_name} {last_name}"
@@ -5,3 +8,5 @@ full_name = f"{first_name} {last_name}"
 print(full_name)
 
 print(f"{first_name.title()} {last_name}")
+
+print("{} {}".format(first_name, last_name))

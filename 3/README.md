e33b70007249cd44a44981ec5cd992e3086feaf0
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-04-26 21:30:50 +0800

    添加元素

--- 3/README.md
+++ 3/README.md
@@ -3,3 +3,5 @@
 ### [访问列表](./list.py)
 
 ### [修改列表](./list-modify.py)
+
+### [添加元素](./list-add.py)
---
+++ 3/list-add.py
@@ -0,0 +1,8 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+
+print(bicycles)
+
+# 在列表末尾添加元素
+bicycles.append('666')
+
+print(bicycles)

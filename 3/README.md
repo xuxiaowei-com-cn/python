2a70e9acf3cf2536458b54e3e890d3ad8489a3c9
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-04-26 21:33:53 +0800

    插入元素

--- 3/README.md
+++ 3/README.md
@@ -5,3 +5,5 @@
 ### [修改列表](./list-modify.py)
 
 ### [添加元素](./list-add.py)
+
+### [插入元素](./list-insert.py)
---
+++ 3/list-insert.py
@@ -0,0 +1,8 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+
+print(bicycles)
+
+# 在列表中插入元素
+bicycles.insert(0, '666')
+
+print(bicycles)
